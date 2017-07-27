d39f46bb12c60fcc3976b3917019c63299b0e2c7
EugeneKovtun <eugene.kraros@gmail.com>
2017-07-27 04:32:41 +0300

    Have been added Constant Message

---
+++ src/main/java/epam/courses/view/ConstantMessage.java
@@ -0,0 +1,25 @@
+package epam.courses.view;
+
+/**
+ * Created by eugene on 27.07.17.
+ */
+public interface ConstantMessage {
+    String INPUT = "Input ";
+    String INTEGER = "integer";
+    String WRONG_INPUT = "Wrong input!";
+    String INDEX = "index (integer value)";
+    String CITY = "city([A-Z][A-z])";
+    String STREET = "street([A-z][A-z])";
+    String HOME = "home number (integer value)";
+    String FLAT = "flat number (integer value)";
+    String NAME = "name[A-Z][a-z]";
+    String SURNAME = "surname[A-Z][a-z]";
+    String PATRONYMIC = "patronymic [A-Z][a-z]";
+    String NICKNAME = "nickname";
+    String HOME_PHONE = "home phone ***-**-**";
+    String MOBILE_PHONE = "mobile phone +**(***)***-**-**";
+    String YES_NO = "1 - yes, 2 - no";
+    String ADD_SECOND_PHONE = "Do you want to add second phone?";
+    String EMAIL = "email";
+    String SKYPE = "skype";
+}

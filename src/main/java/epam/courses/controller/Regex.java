09b17cbffa43189affc378839212452b6569ffd9
EugeneKovtun <eugene.kraros@gmail.com>
2017-07-27 04:33:20 +0300

    Regex have been Added

--- src/main/java/epam/courses/controller/Regex.java
+++ src/main/java/epam/courses/controller/Regex.java
@@ -4,4 +4,11 @@ package epam.courses.controller;
  * Created by eugene on 27.07.17.
  */
 public interface Regex {
+    String NAME_REGEX = "[A-Za-z]+";
+    String NICKNAME_REGEX = "[A-Za-z@_0-9*&^%$#!]+";
+    String HOME_PHONE_NUMBER = "\\d{3}-?\\d{2}-?\\d{2}";
+    String MOBILE_PHONE_NUMBER_REGEX = "\\+?\\d{2}\\(?\\d{3}\\)?\\d{3}-?\\d{2}-?\\d{2}";
+    String EMAIL = "(.+)@(.+)";
+    String SKYPE = "[A-Za-z@_!?0-9\\.]+";
+
 }
